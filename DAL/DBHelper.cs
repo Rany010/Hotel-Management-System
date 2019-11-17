b80fb2baeb4d8846936479526a3240ee060295ac
Rany010 <45391304+Rany010@users.noreply.github.com>
2019-11-17 20:45:58 +0800

    Add files via upload

---
+++ DAL/DBHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+  public class DBHelper
+    {
+         private static string str = "server=(local);integrated security= true;database=HotelManage2";
+      
+      public static DataTable Query(string sql) {
+          SqlConnection conn = new SqlConnection(str);
+          SqlCommand comm = new SqlCommand(sql, conn);
+          SqlDataAdapter da = new SqlDataAdapter(comm);
+          DataTable dt = new DataTable();
+          da.Fill(dt);
+          conn.Close();
+          return dt;
+  
+      }
+
+      public static int Modify(string sql) {
+          SqlConnection conn = new SqlConnection(str);
+          conn.Open();
+          SqlCommand comm = new SqlCommand(sql, conn);
+          int res = comm.ExecuteNonQuery();
+          conn.Close();
+          return res;
+  
+      }
+    }
+}
